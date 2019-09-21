8b50ecd89f1deb5330681a1a20bf2ffebbc55453
Noah Ströhle <drnochi.dev@gmail.com>
2019-09-21 12:02:12 -0700

    Speed up game logic using Zobrist hashing

---
+++ generate_zobrist.py
@@ -0,0 +1,22 @@
+import random
+from dlgo.gotypes import Player, Point
+
+MAX63 = 0x7fffffffffffffff
+
+table = {}
+empty_board = 0
+for row in range(1, 20):
+    for col in range(1, 20):
+        for player in (Player.black, Player.white):
+            code = random.randint(0, MAX63)
+            table[Point(row, col), player] = code
+
+print('from dlgo.gotypes import Player, Point')
+print('')
+print('')
+print('HASH_CODE = {')
+for (pt, p), h in table.items():
+    print('    ({}, {}): {},'.format(pt, p, h))
+print('}')
+print('')
+print('HASH_CODE_EMPTY = {}'.format(empty_board))
